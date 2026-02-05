3eccca762f5edcd6d5b05c74087ff288a69d2067
Arehak <kuteikin_andrei@mail.ru>
2026-02-05 22:58:11 +0300

    Обновить ссылки на скрипты и удалить лишний импорт React

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './app.css';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(HashRouter, { children: _jsx(App, {}) }) }));
+ReactDOM.createRoot(document.getElementById('root')).render(_jsx(HashRouter, { children: _jsx(App, {}) }));
