103ed8d3d9dd92a6a281e49aae5fa826a98be36c
Arehak <kuteikin_andrei@mail.ru>
2026-02-05 23:14:10 +0300

    Обновление маршрутов в компонентах App: замена пути на индексный для компонента Home

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Tickets from './pages/Tickets'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tickets" element={<Tickets />} />
+      <Route index element={<Home />} />
+      <Route path="tickets" element={<Tickets />} />
     </Routes>
   )
 }
