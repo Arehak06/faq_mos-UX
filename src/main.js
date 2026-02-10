42e58bfa81bfa78d0262c3d5147291746a814956
Arehak <kuteikin_andrei@mail.ru>
2026-02-10 13:43:31 +0300

    Beta UI 2.0

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import ReactDOM from 'react-dom/client';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import './tg-ui.css';
+import './app.css';
 ReactDOM.createRoot(document.getElementById('root')).render(_jsx(MemoryRouter, { children: _jsx(App, {}) }));
